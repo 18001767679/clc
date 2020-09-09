fb4f9258455a0fe79c329d351576bf196aad75d7
18001767679 <65285239+18001767679@users.noreply.github.com>
2020-09-09 21:15:26 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+document.body.innerHTML+=`<script src="https://cdn.jsdelivr.net/processing.js/1.4.8/processing.min.js"></script>`
+
 var Game=function(ele){
 	ele.innerHTML=`
 <canvas id="game-canvas-for-${ele.id}"></canvas>
@@ -8,6 +10,13 @@ var Game=function(ele){
 	this.code=document.getElementById("game-coding-area-for-"+ele.id);
 	this.runButton=document.getElementById("game-run-button-for-"+ele.id);
 	this.entity=function(){
-		
+		var sketchProc = function(processingInstance) {
+     		with (processingInstance) {
+        		size(400, 400); 
+        		frameRate(30);
+        		
+    		}
+		};
+    	var processingInstance = new Processing(this.ctx, sketchProc); 
 	}
 }
