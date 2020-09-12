5986109ae7aedf78dc33f580e11c5fd0aae3abb0
18001767679 <65285239+18001767679@users.noreply.github.com>
2020-09-12 20:29:27 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
-document.body.innerHTML+=`<script src="https://cdn.jsdelivr.net/processing.js/1.4.8/processing.min.js"></script>`
-
 var Game=function(ele){
+	var that=this;
 	ele.innerHTML=`
 <canvas id="game-canvas-for-${ele.id}"></canvas>
 <textarea id="game-coding-area-for-${ele.id}"></textarea>
@@ -9,14 +8,36 @@ var Game=function(ele){
 	this.ctx=document.getElementById("game-canvas-for-"+ele.id);
 	this.code=document.getElementById("game-coding-area-for-"+ele.id);
 	this.runButton=document.getElementById("game-run-button-for-"+ele.id);
-	this.entity=function(){
-		var sketchProc = function(processingInstance) {
-     		with (processingInstance) {
-        		size(400, 400); 
-        		frameRate(30);
-        		
-    		}
+	this.entity=function(pos,vel,mass,img){
+		this.game=that;
+		this.pos=pos;
+		this.vel=vel;
+		this.mass=mass;
+		this.img=new Image();
+		this.img.src=img;
+		this.force=function(o){
+			this.vel.add(o);
 		};
-    	var processingInstance = new Processing(this.ctx, sketchProc); 
+		this.update=function(){
+			this.pos.add(this.vel);
+			this.game.ctx.drawImage(this.img,this.pos.x,this.pos.y-this.pos.z/10);
+		};
+	};
+	this.entities=[];
+	this.goals=[];
+	this.runButton.onclick=function(){
+		var th=that;
+		th.run=setInterval(function(){
+			for(let i in th.entities){
+				for(let j in th.entities){
+					if(i==j){
+						continue;
+					}
+					th.entities[i].force(Math.floor(Math.sqrt((th.entities[i].pos.x-th.entities[j].pos.x)*(th.entities[i].pos.x-th.entities[j].pos.x)+(th.entities[i].pos.y-th.entities[j].pos.y)*(th.entities[i].pos.y-th.entities[j].pos.y)+(th.entities[i].pos.y-th.entities[j].pos.y)*(th.entities[i].pos.y-th.entities[j].pos.y))));
+				}
+				th.entities[i].update();
+			}
+		},40);
 	}
 }
+document.body.innerHTML+=`<script src="/physics.js"></script>`
